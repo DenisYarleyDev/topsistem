1431072bec44258986897992a0436a75ddc16de7
Kaio <kaio.franklin.dev@gmail.com>
2025-05-22 05:15:12 -0300

    passando validação de usuario logado para ProtectRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,14 +1,51 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { backUrl } from "./Constants";
+import axios from "axios";
+import { useEffect } from "react";
 
 //FRONT COMPONENT PROTECTED ROUTES
 export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
 
-  //desativar se quer acessar paginas protegidas por token
-  // if (!token) {
-  //   return <Navigate to={"/"} />;
-  // } else {
-  //   return children;
-  // }
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let intervalId; // sem anotação de tipo
+
+    async function verifyJWT() {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        navigate("/");
+        return;
+      }
+
+      try {
+        const response = await axios.post(
+          `${backUrl}/verifyJWT`,
+          {},
+          {
+            headers: { "x-access-token": token },
+          }
+        );
+
+        if (!response.data.valid) {
+          navigate("/");
+        }
+      } catch (err) {
+        console.error("Erro ao verificar token:", err);
+        navigate("/");
+      }
+    }
+
+    // chama na montagem
+    verifyJWT();
+
+    // agenda execução a cada 10s
+    intervalId = setInterval(verifyJWT, 10_000);
+
+    // cleanup ao desmontar
+    return () => clearInterval(intervalId);
+  }, [navigate]);
+
   return children;
 }
